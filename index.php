d86aadb7f8a2a3f30c78b609d24dd0e7dfe59fc8
Yury Gubsky <yury.gubsky@gmail.com>
2018-07-30 00:59:38 +0400

    Add Order Book and Open Orders

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ require 'conf/main.php';
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
-	<title>API Idex-Market</title>
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="author" content="Yuri Gubsky">
+	<meta name="keywords" content="bot, бот, idex.market">
+	<meta name="description" content="bot, бот">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Idex.Market - Бот</title>
 	<link rel="stylesheet" href="css/styles.css<?= $conf['cache'] ?>">
 	<link rel="stylesheet" href="css/datatables.css">
 </head>
@@ -18,24 +23,26 @@ require 'conf/main.php';
 	<div class="content">
 		<div class="section">
 			<div class="subsection">
-				<button data-fn="returnOrderBook" data-market="ETH_PAL">OrderBook: ETH_PAL</button>
-				<button data-fn="returnOrderBookAndry" data-market="ETH_PAL" data-address="0x8899af1aa48cdfdedbf394221ab5fb9b69f4ae7b">OrderBook: ETH_PAL Andry</button>
-				<div class="TickerAll">
-					<div class="title">Список монет</div>
-					<div class="list"></div>
-				</div>
+				<div class="title">Список монет</div>
+				<div class="ticker" data-selected-coin=""></div>
 			</div>
 			<div class="subsection margin-left">
 				<div>
 					<div class="title">Настройки</div>
+					<button class="btn-order-book">Список всех ордеров</button>
+				    <button class="btn-open-orders" data-address="0x8899af1aa48cdfdedbf394221ab5fb9b69f4ae7b">Список открытых ордеров</button>
 				</div>
 			</div>
 		</div>
 		<div class="section">
 			<div class="subsection">
-				<div class="result"></div>
+				<div class="title">Продажа</div>
+				<div class="orders-asks"></div>
 			</div>
-			<div class="subsection margin-left"></div>
+			<div class="subsection margin-left">
+				<div class="title">Покупка</div>
+				<div class="orders-bids"></div>
+			</div>
 		</div>
 	</div>
 	<div class="footer"></div>
--- js/main.js
+++ js/main.js
@@ -2,74 +2,20 @@
 
 let private_key = '0xfb9ff9d3ddd0e371bd1d32704cc21d834f3a932749324e3eb0a2d9558702071a';
 
-returnTickerAll();
-
-$('button').click(function () {
-	window[$(this).attr('data-fn')]($(this));
-});
-
+// Основные настройки ajax запросов
 $.ajaxSetup({
 	method: 'post',
 	dataType: 'json'
 });
 
+// Обработчик ошибок ajax запросов
 $(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
 	alert('Error!\n' + 'URL: ' + ajaxSettings.url);
 });
 
-function substrCoin(coin) {
-	return coin.substr(4);
-}
+getTicker();
 
-function substrPercent(percent) {
-	let result      = '';
-	let indexHyphen = percent.indexOf('-');
-	let indexDot    = percent.indexOf('.');
-
-	// Если найден дефис и не найдена точка
-	if (indexHyphen !== -1 && indexDot == -1) {
-		result = '<span class="color-red">' + percent.substr(0, 3) + '%</span>';
-	}
-	// Если не найден дефис и найдена точка
-	else if (indexHyphen == -1 && indexDot !== -1) {
-		result = '<span class="color-green">+' + percent.substr(0, indexDot + 3) + '%</span>';
-	}
-	// Если найден дефис и точка
-	else if (indexHyphen !== -1 && indexDot !== -1) {
-		result = '<span class="color-red">' + percent.substr(0, indexDot + 3) + '%</span>';
-	}
-	// Если не найден дефис и точка
-	else if (indexHyphen !== -1 && indexDot !== -1) {
-		result = '<span class="color-green">+' + percent.substr(0, 2) + '%</span>';
-	}
-	else if (percent == 0) {
-		result = percent + '%';
-	}
-
-	return result;
-}
-
-function substrBaseVolume(volume) {
-	let result      = '';
-	let indexDot    = volume.indexOf('.');
-
-	// Если не найдена точка
-	if (indexDot == -1) {
-		result = volume;
-	}
-	// Если найдена точка
-	else if (indexDot !== -1) {
-		result = volume.substr(0, indexDot + 3);
-	}
-	// Если значение равно 0
-	else if (volume == 0) {
-		result = volume;
-	}
-
-	return result;
-}
-
-function returnTickerAll() {	
+function getTicker() {
 	$.ajax({
 		url: 'https://api.idex.market/returnTicker'
 	})
@@ -110,12 +56,11 @@ function returnTickerAll() {
 			html += '</tr>';
 		});
 
-		html +=
-		'</tbody>' +
-		'</table>';
+		html += '</tbody>';
+		html += '</table>';
 
-		$('.TickerAll > .list').html(html);
-		$('.TickerAll > .list > table').DataTable({
+		$('.ticker').html(html);
+		$('.ticker > table').DataTable({
 			'order': [[0, 'asc']],
 			'searching': true,
 			'ordering': true,
@@ -181,13 +126,22 @@ function returnTickerAll() {
 	});
 };
 
-$('.TickerAll > .list').on('click', '.dataTables_wrapper > .dataTables_scroll > .dataTables_scrollBody > table > tbody > tr', function() {
-	alert($(this).attr('data-coin'));
+$('.ticker').on('click', '.dataTables_wrapper > .dataTables_scroll > .dataTables_scrollBody > table > tbody > tr', function() {
+	$('.ticker').attr('data-selected-coin', $(this).attr('data-coin'));
 });
 
-function returnOrderBook(e) {
+$('.btn-order-book').click(function() {
+	if (getSelectedCoin()) {
+		getOrderBook(getSelectedCoin());
+	}
+	else {
+		alert('Сначала выберите валютную пару');
+	}
+});
+
+function getOrderBook(coin) {
 	let request = {
-		market: e.attr('data-market')
+		market: coin
 	};
 
 	$.ajax({
@@ -195,50 +149,127 @@ function returnOrderBook(e) {
 		data: JSON.stringify(request)
 	})
 	.done(function(data, textStatus, jqXHR) {
-		let html = '';
-
-		html += '<b>ASKS</b><br>';
+		let ordersAsks =
+		'<table class="display compact cell-border" style="width: 100%">' +
+		'<thead>' +
+		'    <tr>' +
+		'        <th>Цена</th>' +
+		'        <th>' + substrCoin(coin) + '</th>' +
+		'        <th>ETH</th>' +
+		'    </tr>' +
+		'</thead>' +
+		'<tbody>';
 
 		$.each(data.asks, function(i1, v1) {
+			ordersAsks += '<tr>';
+
 			$.each(v1, function(i2, v2) {
 				if (i2 == 'params') {
-					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					/* ordersAsks += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
 					$.each(v2, function(i3, v3) {
-						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
-					});
+						ordersAsks += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					}); */
 				}
-				else {
-					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				else if (i2 == 'price') {
+					ordersAsks += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'amount') {
+					ordersAsks += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'total') {
+					ordersAsks += '<td>' + v2 + '</td>';
 				}
 			});
-			html += '<hr>';
+
+			ordersAsks += '</tr>';
 		});
 
-		html += '<b>BIDS</b><br>';
+		ordersAsks += '</tbody>';
+		ordersAsks += '</table>';
+
+		$('.orders-asks').html(ordersAsks);
+		$('.orders-asks > table').DataTable({
+			'order': [[0, 'asc']],
+			'searching': false,
+			'ordering': false,
+			'scrollX': true,
+			'scrollY': '300px',
+			'scrollCollapse': true,
+			'info': false,
+			'paging': false,
+			'language': {
+				'url': 'conf/DataTables_RU.json'
+			}
+		});
+
+		let ordersBids =
+		'<table class="display compact cell-border" style="width: 100%">' +
+		'<thead>' +
+		'    <tr>' +
+		'        <th>Цена</th>' +
+		'        <th>' + substrCoin(coin) + '</th>' +
+		'        <th>ETH</th>' +
+		'    </tr>' +
+		'</thead>' +
+		'<tbody>';
 
 		$.each(data.bids, function(i1, v1) {
+			ordersBids += '<tr>';
+
 			$.each(v1, function(i2, v2) {
 				if (i2 == 'params') {
-					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					/* ordersBids += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
 					$.each(v2, function(i3, v3) {
-						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
-					});
+						ordersBids += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					}); */
 				}
-				else {
-					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				else if (i2 == 'price') {
+					ordersBids += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'amount') {
+					ordersBids += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'total') {
+					ordersBids += '<td>' + v2 + '</td>';
 				}
 			});
-			html += '<hr>';
+
+			ordersBids += '</tr>';
 		});
 
-		$('.result').html(html);
+		ordersBids += '</tbody>';
+		ordersBids += '</table>';
+
+		$('.orders-bids').html(ordersBids);
+		$('.orders-bids > table').DataTable({
+			'order': [[0, 'asc']],
+			'searching': false,
+			'ordering': false,
+			'scrollX': true,
+			'scrollY': '300px',
+			'scrollCollapse': true,
+			'info': false,
+			'paging': false,
+			'language': {
+				'url': 'conf/DataTables_RU.json'
+			}
+		});
 	});
 }
 
-function returnOrderBookAndry(e) {
+$('.btn-open-orders').click(function() {
+	if (getSelectedCoin()) {
+		getOpenOrders(getSelectedCoin(), $(this).attr('data-address'));
+	}
+	else {
+		alert('Сначала выберите валютную пару');
+	}
+});
+
+function getOpenOrders(coin, address) {
 	let request = {
-		market: e.attr('data-market'),
-		address: e.attr('data-address')
+		market: coin,
+		address: address
 	};
 
 	$.ajax({
@@ -246,42 +277,172 @@ function returnOrderBookAndry(e) {
 		data: JSON.stringify(request)
 	})
 	.done(function(data, textStatus, jqXHR) {
-		let html = '';
-
-		html += '<b>ASKS</b><br>';
+		let ordersAsks =
+		'<table class="display compact cell-border" style="width: 100%">' +
+		'<thead>' +
+		'    <tr>' +
+		'        <th>Цена</th>' +
+		'        <th>' + substrCoin(coin) + '</th>' +
+		'        <th>ETH</th>' +
+		'    </tr>' +
+		'</thead>' +
+		'<tbody>';
 
 		$.each(data.asks, function(i1, v1) {
+			ordersAsks += '<tr>';
+
 			$.each(v1, function(i2, v2) {
 				if (i2 == 'params') {
-					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					/* ordersAsks += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
 					$.each(v2, function(i3, v3) {
-						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
-					});
+						ordersAsks += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					}); */
 				}
-				else {
-					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				else if (i2 == 'price') {
+					ordersAsks += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'amount') {
+					ordersAsks += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'total') {
+					ordersAsks += '<td>' + v2 + '</td>';
 				}
 			});
-			html += '<hr>';
+
+			ordersAsks += '</tr>';
 		});
 
-		html += '<b>BIDS</b><br>';
+		ordersAsks += '</tbody>';
+		ordersAsks += '</table>';
+
+		$('.orders-asks').html(ordersAsks);
+		$('.orders-asks > table').DataTable({
+			'order': [[0, 'asc']],
+			'searching': false,
+			'ordering': false,
+			'scrollX': true,
+			'scrollY': '300px',
+			'scrollCollapse': true,
+			'info': false,
+			'paging': false,
+			'language': {
+				'url': 'conf/DataTables_RU.json'
+			}
+		});
+
+		let ordersBids =
+		'<table class="display compact cell-border" style="width: 100%">' +
+		'<thead>' +
+		'    <tr>' +
+		'        <th>Цена</th>' +
+		'        <th>' + substrCoin(coin) + '</th>' +
+		'        <th>ETH</th>' +
+		'    </tr>' +
+		'</thead>' +
+		'<tbody>';
 
 		$.each(data.bids, function(i1, v1) {
+			ordersBids += '<tr>';
+
 			$.each(v1, function(i2, v2) {
 				if (i2 == 'params') {
-					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					/* ordersBids += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
 					$.each(v2, function(i3, v3) {
-						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
-					});
+						ordersBids += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					}); */
 				}
-				else {
-					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				else if (i2 == 'price') {
+					ordersBids += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'amount') {
+					ordersBids += '<td>' + v2 + '</td>';
+				}
+				else if (i2 == 'total') {
+					ordersBids += '<td>' + v2 + '</td>';
 				}
 			});
-			html += '<hr>';
+
+			ordersBids += '</tr>';
 		});
 
-		$('.result').html(html);
+		ordersBids += '</tbody>';
+		ordersBids += '</table>';
+
+		$('.orders-bids').html(ordersBids);
+		$('.orders-bids > table').DataTable({
+			'order': [[0, 'asc']],
+			'searching': false,
+			'ordering': false,
+			'scrollX': true,
+			'scrollY': '300px',
+			'scrollCollapse': true,
+			'info': false,
+			'paging': false,
+			'language': {
+				'url': 'conf/DataTables_RU.json'
+			}
+		});
 	});
+}
+
+function substrCoin(coin) {
+	return coin.substr(4);
+}
+
+function substrPercent(percent) {
+	let result      = '';
+	let indexHyphen = percent.indexOf('-');
+	let indexDot    = percent.indexOf('.');
+
+	// Если найден дефис и не найдена точка
+	if (indexHyphen !== -1 && indexDot == -1) {
+		result = '<span class="color-red">' + percent.substr(0, 3) + '%</span>';
+	}
+	// Если не найден дефис и найдена точка
+	else if (indexHyphen == -1 && indexDot !== -1) {
+		result = '<span class="color-green">+' + percent.substr(0, indexDot + 3) + '%</span>';
+	}
+	// Если найден дефис и точка
+	else if (indexHyphen !== -1 && indexDot !== -1) {
+		result = '<span class="color-red">' + percent.substr(0, indexDot + 3) + '%</span>';
+	}
+	// Если не найден дефис и точка
+	else if (indexHyphen !== -1 && indexDot !== -1) {
+		result = '<span class="color-green">+' + percent.substr(0, 2) + '%</span>';
+	}
+	else if (percent == 0) {
+		result = percent + '%';
+	}
+
+	return result;
+}
+
+function substrBaseVolume(volume) {
+	let result      = '';
+	let indexDot    = volume.indexOf('.');
+
+	// Если не найдена точка
+	if (indexDot == -1) {
+		result = volume;
+	}
+	// Если найдена точка
+	else if (indexDot !== -1) {
+		result = volume.substr(0, indexDot + 3);
+	}
+	// Если значение равно 0
+	else if (volume == 0) {
+		result = volume;
+	}
+
+	return result;
+}
+
+function getSelectedCoin() {
+	let coin = $('.ticker').attr('data-selected-coin');
+
+	if (coin == '') {
+		coin = false;
+	}
+
+	return coin;
 }
