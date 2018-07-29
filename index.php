8b3bdba27b35be8fa7d7635871d69dc686a2ef6d
Yury Gubsky <yury.gubsky@gmail.com>
2018-07-29 12:28:31 +0400

    Add header

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
+<?php
+require 'conf/main.php';
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
 	<title>API Idex-Market</title>
-	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/styles.css<?= $conf['cache'] ?>">
 </head>
 <body>
-	<h1>API Idex-Market</h1><hr>
-	<button>Send request</button>
+	<div class="header">
+		<a href="/projects/idex-market/" class="logo">
+			<img src="images/logo-idex.svg" alt="Logo">
+		</a>
+	</div>
+	<div class="container">
+		<button data-fn="returnTickerAll">Ticker: All</button>
+		<button data-fn="return24Volume">Ticker: 24 hour</button>
+		<button data-fn="returnOrderBook" data-market="ETH_PAL">OrderBook: ETH_PAL</button>
+		<button data-fn="returnOrderBookAndry" data-market="ETH_PAL" data-address="0x8899af1aa48cdfdedbf394221ab5fb9b69f4ae7b">OrderBook: ETH_PAL Andry</button>
+		<div class="result"></div>
+	</div>
 	<script src="js/jquery.js"></script>
-	<script src="js/main.js"></script>
+	<script src="js/main.js<?= $conf['cache'] ?>"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,21 +1,166 @@
 'use strict'
 
+let private_key = '0xfb9ff9d3ddd0e371bd1d32704cc21d834f3a932749324e3eb0a2d9558702071a';
+
 $('button').click(function () {
-	$.ajax({
-		method: 'post',
-		dataType: 'json',
-		url: 'conf/main.json',
-		data: {
-			action: 'test'
-		},
-		done: function(response) {
-			console.log(response);
-		},
-		fail: function() {
-			console.log();
-		},
-		always: function() {
-			console.log('Always msg');
-		}
-	});
+	window[$(this).attr('data-fn')]($(this));
 });
+
+$.ajaxSetup({
+	method: 'post',
+	dataType: 'json'
+});
+
+$(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
+	alert('Error!\n' + 'URL: ' + ajaxSettings.url);
+});
+
+function returnTickerAll(e) {
+	$.ajax({
+		url: 'https://api.idex.market/returnTicker'
+	})
+	.done(function(data, textStatus, jqXHR) {
+		let html = '';
+
+		$.each(data, function(i1, v1) {
+			html += '<b>' + i1 + '</b><br>';
+
+			$.each(v1, function(i2, v2) {
+				html += i2 + ': ' + v2 + '<br>';
+			});
+
+			html += '<hr>';
+		});
+
+		$('.result').html(html);
+	});
+}
+
+function return24Volume(e) {
+	$.ajax({
+		url: 'https://api.idex.market/return24Volume',
+	})
+	.done(function(data, textStatus, jqXHR) {
+		let html = '';
+
+		$.each(data, function(i1, v1) {
+			if (i1 == 'totalETH') {
+				html += '<b>' + i1 + '</b>: ' + v1 + '<hr>';
+				return false;
+			}
+		});
+
+		$.each(data, function(i1, v1) {
+			if (i1 !== 'totalETH') {
+				html += '<b>' + i1 + '</b><br>';
+
+				$.each(v1, function(i2, v2) {
+					html += '&nbsp;&nbsp;&nbsp;' + i2 + ': ' + v2 + '<br>';
+				});
+			}
+		});
+
+		$('.result').html(html);
+	});
+}
+
+function returnOrderBook(e) {
+	let request = {
+		market: e.attr('data-market')
+	};
+
+	$.ajax({
+		url: 'https://api.idex.market/returnOrderBook',
+		data: JSON.stringify(request)
+	})
+	.done(function(data, textStatus, jqXHR) {
+		let html = '';
+
+		html += '<b>ASKS</b><br>';
+
+		$.each(data.asks, function(i1, v1) {
+			$.each(v1, function(i2, v2) {
+				if (i2 == 'params') {
+					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					$.each(v2, function(i3, v3) {
+						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					});
+				}
+				else {
+					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				}
+			});
+			html += '<hr>';
+		});
+
+		html += '<b>BIDS</b><br>';
+
+		$.each(data.bids, function(i1, v1) {
+			$.each(v1, function(i2, v2) {
+				if (i2 == 'params') {
+					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					$.each(v2, function(i3, v3) {
+						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					});
+				}
+				else {
+					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				}
+			});
+			html += '<hr>';
+		});
+
+		$('.result').html(html);
+	});
+}
+
+function returnOrderBookAndry(e) {
+	let request = {
+		market: e.attr('data-market'),
+		address: e.attr('data-address')
+	};
+
+	$.ajax({
+		url: 'https://api.idex.market/returnOrderBook',
+		data: JSON.stringify(request)
+	})
+	.done(function(data, textStatus, jqXHR) {
+		let html = '';
+
+		html += '<b>ASKS</b><br>';
+
+		$.each(data.asks, function(i1, v1) {
+			$.each(v1, function(i2, v2) {
+				if (i2 == 'params') {
+					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					$.each(v2, function(i3, v3) {
+						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					});
+				}
+				else {
+					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				}
+			});
+			html += '<hr>';
+		});
+
+		html += '<b>BIDS</b><br>';
+
+		$.each(data.bids, function(i1, v1) {
+			$.each(v1, function(i2, v2) {
+				if (i2 == 'params') {
+					html += '&nbsp;&nbsp;&nbsp;<b>params:</b><br>';
+					$.each(v2, function(i3, v3) {
+						html += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + i3 + ': ' + v3 + '<br>';
+					});
+				}
+				else {
+					html += '&nbsp;&nbsp;&nbsp;' + i2 + ':' + v2 + '<br>';
+				}
+			});
+			html += '<hr>';
+		});
+
+		$('.result').html(html);
+	});
+}
