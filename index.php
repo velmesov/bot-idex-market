346152116d48c0bf17fe144a385aef0029e316a7
Yury Gubsky <yury.gubsky@gmail.com>
2018-07-29 22:49:32 +0400

    Add DataTable

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require 'conf/main.php';
 	<meta charset="UTF-8">
 	<title>API Idex-Market</title>
 	<link rel="stylesheet" href="css/styles.css<?= $conf['cache'] ?>">
+	<link rel="stylesheet" href="css/datatables.css">
 </head>
 <body>
 	<div class="header">
@@ -14,14 +15,32 @@ require 'conf/main.php';
 			<img src="images/logo-idex.svg" alt="Logo">
 		</a>
 	</div>
-	<div class="container">
-		<button data-fn="returnTickerAll">Ticker: All</button>
-		<button data-fn="return24Volume">Ticker: 24 hour</button>
-		<button data-fn="returnOrderBook" data-market="ETH_PAL">OrderBook: ETH_PAL</button>
-		<button data-fn="returnOrderBookAndry" data-market="ETH_PAL" data-address="0x8899af1aa48cdfdedbf394221ab5fb9b69f4ae7b">OrderBook: ETH_PAL Andry</button>
-		<div class="result"></div>
+	<div class="content">
+		<div class="section">
+			<div class="subsection">
+				<button data-fn="returnOrderBook" data-market="ETH_PAL">OrderBook: ETH_PAL</button>
+				<button data-fn="returnOrderBookAndry" data-market="ETH_PAL" data-address="0x8899af1aa48cdfdedbf394221ab5fb9b69f4ae7b">OrderBook: ETH_PAL Andry</button>
+				<div class="TickerAll">
+					<div class="title">Список монет</div>
+					<div class="list"></div>
+				</div>
+			</div>
+			<div class="subsection margin-left">
+				<div>
+					<div class="title">Настройки</div>
+				</div>
+			</div>
+		</div>
+		<div class="section">
+			<div class="subsection">
+				<div class="result"></div>
+			</div>
+			<div class="subsection margin-left"></div>
+		</div>
 	</div>
+	<div class="footer"></div>
 	<script src="js/jquery.js"></script>
+	<script src="js/datatables.js"></script>
 	<script src="js/main.js<?= $conf['cache'] ?>"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 let private_key = '0xfb9ff9d3ddd0e371bd1d32704cc21d834f3a932749324e3eb0a2d9558702071a';
 
+returnTickerAll();
+
 $('button').click(function () {
 	window[$(this).attr('data-fn')]($(this));
 });
@@ -15,54 +17,173 @@ $(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
 	alert('Error!\n' + 'URL: ' + ajaxSettings.url);
 });
 
-function returnTickerAll(e) {
+function substrCoin(coin) {
+	return coin.substr(4);
+}
+
+function substrPercent(percent) {
+	let result      = '';
+	let indexHyphen = percent.indexOf('-');
+	let indexDot    = percent.indexOf('.');
+
+	// Если найден дефис и не найдена точка
+	if (indexHyphen !== -1 && indexDot == -1) {
+		result = '<span class="color-red">' + percent.substr(0, 3) + '%</span>';
+	}
+	// Если не найден дефис и найдена точка
+	else if (indexHyphen == -1 && indexDot !== -1) {
+		result = '<span class="color-green">+' + percent.substr(0, indexDot + 3) + '%</span>';
+	}
+	// Если найден дефис и точка
+	else if (indexHyphen !== -1 && indexDot !== -1) {
+		result = '<span class="color-red">' + percent.substr(0, indexDot + 3) + '%</span>';
+	}
+	// Если не найден дефис и точка
+	else if (indexHyphen !== -1 && indexDot !== -1) {
+		result = '<span class="color-green">+' + percent.substr(0, 2) + '%</span>';
+	}
+	else if (percent == 0) {
+		result = percent + '%';
+	}
+
+	return result;
+}
+
+function substrBaseVolume(volume) {
+	let result      = '';
+	let indexDot    = volume.indexOf('.');
+
+	// Если не найдена точка
+	if (indexDot == -1) {
+		result = volume;
+	}
+	// Если найдена точка
+	else if (indexDot !== -1) {
+		result = volume.substr(0, indexDot + 3);
+	}
+	// Если значение равно 0
+	else if (volume == 0) {
+		result = volume;
+	}
+
+	return result;
+}
+
+function returnTickerAll() {	
 	$.ajax({
 		url: 'https://api.idex.market/returnTicker'
 	})
 	.done(function(data, textStatus, jqXHR) {
-		let html = '';
+		let html =
+		'<table class="display compact cell-border" style="width: 100%">' +
+		'<thead>' +
+		'    <tr>' +
+		'        <th>Монета</th>' +
+		'        <th>Цена</th>' +
+		'        <th>High</th>' +
+		'        <th>Low</th>' +
+		'        <th>Lowest Ask</th>' +
+		'        <th>Highest Bid</th>' +
+		'        <th>Процент изменения</th>' +
+		'        <th>Объём</th>' +
+		'        <th>Quote Volume</th>' +
+		'    </tr>' +
+		'</thead>' +
+		'<tbody>';
 
 		$.each(data, function(i1, v1) {
-			html += '<b>' + i1 + '</b><br>';
+			html += '<tr data-coin="' + i1 + '">'
+			html += '    <td>' + substrCoin(i1) + '</td>';
 
 			$.each(v1, function(i2, v2) {
-				html += i2 + ': ' + v2 + '<br>';
+				if (i2 == 'percentChange') {
+					html += '<td>' + substrPercent(v2) + '</td>';
+				}
+				else if (i2 == 'baseVolume') {
+					html += '<td>' + substrBaseVolume(v2) + '</td>';
+				}
+				else {
+					html += '<td>' + v2 + '</td>';
+				}
 			});
 
-			html += '<hr>';
+			html += '</tr>';
 		});
 
-		$('.result').html(html);
+		html +=
+		'</tbody>' +
+		'</table>';
+
+		$('.TickerAll > .list').html(html);
+		$('.TickerAll > .list > table').DataTable({
+			'order': [[0, 'asc']],
+			'searching': true,
+			'ordering': true,
+			'scrollX': true,
+			'scrollY': '300px',
+			'scrollCollapse': true,
+			'info': false,
+			'paging': false,
+			/* 'fixedColumns': {
+				'leftColumns': 1
+			}, */
+			'language': {
+				'url': 'conf/DataTables_RU.json'
+			},
+			'columnDefs': [
+				{
+					'targets': [0],
+					'visible': true,
+					'searchable': true
+				},
+				{
+					'targets': [1],
+					'visible': true,
+					'searchable': false
+				},
+				{
+					'targets': [2],
+					'visible': false,
+					'searchable': false
+				},
+				{
+					'targets': [3],
+					'visible': false,
+					'searchable': false
+				},
+				{
+					'targets': [4],
+					'visible': false,
+					'searchable': false
+				},
+				{
+					'targets': [5],
+					'visible': false,
+					'searchable': false
+				},
+				{
+					'targets': [6],
+					'visible': true,
+					'searchable': false
+				},
+				{
+					'targets': [7],
+					'visible': true,
+					'searchable': false
+				},
+				{
+					'targets': [8],
+					'visible': false,
+					'searchable': false
+				},
+			]
+		});
 	});
-}
+};
 
-function return24Volume(e) {
-	$.ajax({
-		url: 'https://api.idex.market/return24Volume',
-	})
-	.done(function(data, textStatus, jqXHR) {
-		let html = '';
-
-		$.each(data, function(i1, v1) {
-			if (i1 == 'totalETH') {
-				html += '<b>' + i1 + '</b>: ' + v1 + '<hr>';
-				return false;
-			}
-		});
-
-		$.each(data, function(i1, v1) {
-			if (i1 !== 'totalETH') {
-				html += '<b>' + i1 + '</b><br>';
-
-				$.each(v1, function(i2, v2) {
-					html += '&nbsp;&nbsp;&nbsp;' + i2 + ': ' + v2 + '<br>';
-				});
-			}
-		});
-
-		$('.result').html(html);
-	});
-}
+$('.TickerAll > .list').on('click', '.dataTables_wrapper > .dataTables_scroll > .dataTables_scrollBody > table > tbody > tr', function() {
+	alert($(this).attr('data-coin'));
+});
 
 function returnOrderBook(e) {
 	let request = {
